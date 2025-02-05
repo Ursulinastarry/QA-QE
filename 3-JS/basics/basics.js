d3351eecaba98104b9bbabd13eb5094fa4e5ca2a
Ursulinastarry <paulinawambua8@gmail.com>
2025-02-05 14:11:10 +0300

    changed  file paths

---
+++ 3-JS/basics/basics.js
@@ -0,0 +1,5 @@
+import chalk from "chalk"
+console.log(chalk.yellow("Hey"));
+console.log("how are you?")
+const num=[2,3,4]
+console.table(num)
---
+++ 3-JS/basics/variables/defining.js
@@ -0,0 +1,21 @@
+import chalk from "chalk"
+let name="lina"
+console.log(chalk.blue(name))
+const marks='23'
+console.log(parseInt(marks))
+console.log(typeof '23')
+console.log(marks)
+console.log(4)
+console.log(typeof 4)
+
+const numIsBig=12344566788865343244566n
+console.log(numIsBig)
+const isAuthenticated = true
+const isAdmin = false
+//object
+let myData={}
+console.log(myData)
+console.log(typeof myData)
+/*myData.name="linaaa"
+muData.uni="dkut"
+console.table(myData)*/

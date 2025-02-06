0e1c84d0c1b11208bb6419a2db57db7c3bf5fe75
Ursulinastarry <paulinawambua8@gmail.com>
2025-02-06 12:04:24 +0300

    added a folder in 2-CSS

--- 3-JS/basics/variables/defining.js
+++ 3-JS/basics/variables/defining.js
@@ -19,3 +19,44 @@ console.log(typeof myData)
 myData.name="linaaa"
 myData.uni="dkut"
 console.table(myData)
+
+//strings
+//1.length
+console.log("brahh".length)
+//charAt()-char a index in word
+console.log("brahh".charAt(3))
+//concat -joining 2 strings
+console.log("brahh".concat(' ' + "bebi"))
+//indexOf-first occurrence of char
+console.log("brahh".indexOf("a"))
+//includes
+console.log("brahh".includes("a"))//true
+console.log("brahh".includes("A"))//false
+//toLowerCase   toUpperCase
+let name2="LINA2"
+console.log(name2.toLowerCase())
+//reverse-split first
+let name3="LINA"
+console.log(name3.split(""))
+//substring-extracts characters from string in range index
+console.log("i am a girl".substring(5,8))//index5-8
+//substr
+console.log("i am a girl".substr(8,2))//from index 8,2 characters
+//trim-remove white spaces frm both ends of string
+console.log('    brrrrr    ')
+console.log('    brrrrr    '.trim())//trimStart-trims left space,trimEnd
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
